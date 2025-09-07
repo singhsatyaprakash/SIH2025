693063b4b1a1a3fc2a5d88725857f73cef26f605
Satya Prakashh Singh <singhsatyaprakash70675.com>
2025-09-07 16:56:18 +0530

    year

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -41,7 +41,7 @@ const Footer = () => {
 
       {/* Bottom */}
       <div className="text-center text-sm text-gray-500 mt-6">
-        © 2024 SafeTour India. All rights reserved.
+        © 2025 SafeTour India. All rights reserved.
       </div>
     </footer>
   );
